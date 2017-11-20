724cb668b0618311ffab757f1e438fdd99eb8732
Marcos Vinícius C. da Silva <marvinc.silva@gmail.com>
2017-11-20 17:03:45 -0200

    use a doubled linked list to represent the timeline

--- src/domain/models/Timeline/Generation.js
+++ src/domain/models/Timeline/Generation.js
@@ -2,18 +2,39 @@ function isAlive(cell) {
   return cell.isAlive
 }
 
+function getGameOverReason(generation) {
+  if (generation.gameOver) return generation.gameOver
+
+  if (!generation.cells.some(isAlive)) return 'EMPTY_BOARD'
+
+  return null
+}
+
 class Generation {
-  constructor({ cells }) {
+  constructor({ cells, previous }) {
     this.cells = cells.filter(isAlive)
+    this.previous = previous
+    this.next = null
+
+    this.number = previous ? previous.number + 1 : 0
   }
 
-  get canGoNext() {
-    return this.cells.some(isAlive)
+  get isFirst() {
+    return !this.previous
   }
 
-  //  eslint-disable-next-line class-methods-use-this
-  next() {
-    return new Generation({ cells: [] })
+  get isLast() {
+    return !this.next
+  }
+
+  get gameOverReason() {
+    this.gameOver = getGameOverReason(this)
+
+    return this.gameOver
+  }
+
+  createNext() {
+    return new Generation({ cells: [], previous: this })
   }
 }
 
